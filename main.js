4788dd37b94e0bd5fbd2b3ae3ca0dedad0970afa
CLAUDIO HENRIQUE ROCHA DA SILVA <claudio.hrsilva@senacsp.edu.br>
2025-02-20 19:55:35 -0300

    adicionando clientes

--- main.js
+++ main.js
@@ -4,11 +4,32 @@
  */
 
 // importação do módulo de coneção (database.js)
-const {conectar, desconectar} = require('./database')
+const { conectar, desconectar } = require('./database.js')
+
+// importação do modelo de dados de clientes
+const clienteModel = require('./src/models/clientes.js')
+
+// CRUD Create (função para adicionar um novo cliente)
+const criarCliente = async (nomeCli, foneCli) => {
+    try {
+        const novoCliente = new clienteModel(
+            {
+                nomeCliente: nomeCli,
+                foneCliente: foneCli
+            }
+        )
+        // a linha abaixo salva os dados do cliente no banco
+        await novoCliente.save()
+        console.log("Cliente adicionado com sucesso.")
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 // execução da aplicação
 const app = async () => {
     await conectar()
+    await criarCliente("Yvis da Massa", "1193541-6121")
     await desconectar()
 }
 
